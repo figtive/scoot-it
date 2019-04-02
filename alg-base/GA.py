d1ae39700730a67b188fc95bc4e247b069e3845f
Danny August Ramaputra <august_danny@yahoo.co.id>
2019-04-02 08:29:41 +0700

    added base classes

---
+++ alg-base/GA.py
@@ -0,0 +1,27 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+
+class Gene:
+    # Gene class, has point coords
+
+
+class Chromosome:
+    # Chromosome class, has sequence of Genes
+
+
+class Population:
+    # Population class, has multiple Chromosomes
+
+
+class GA:
+    # GA class, runs GA through Populations
+
+def main():
+    np.random.seed(1)
+    random.seed(1)
+
+
+if __name__ == "__main__":
+    main()
